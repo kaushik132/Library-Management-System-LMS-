d098af63101bb4d99dd31f095c49914e13bcbb29
kaushik132 <kaushik.dey.ahit@gmail.com>
2024-10-22 10:34:58 +0530

    new update

--- config/admin.php
+++ config/admin.php
@@ -410,6 +410,15 @@ return [
     |
     */
     'extensions' => [
+'ckeditor' => [
 
+            //Set to false if you want to disable this extension
+            'enable' => true,
+
+            // Editor configuration
+            'config' => [
+
+            ]
+        ]
     ],
 ];
